ae9f1c3d33a9b21eb9dd9d3006b3667ed8efe4b2
吉見自然 <yoshimi@yoshimishizen-no-MacBook-Air.local>
2018-08-05 01:06:46 +0900

    Improve

--- index.js
+++ index.js
@@ -60,6 +60,10 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
                 type: "text",
                 text: "画像ですな！"
             }));
+            events_processed.push(bot.replyMessage(event.replyToken, {
+                type: "text",
+                text: "画像ですな２！"
+            }));
         }
         if (event.type == "message" && event.message.type == "file"){
             events_processed.push(bot.replyMessage(event.replyToken, require("./confirm-button.json")));
@@ -72,12 +76,12 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
                 type: "text",
                 text: "ファイルですな！"
             }));
-            if(event.message.fileName.includes("m4a")){
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "いい音ですので保存しておきますぞ！"
-                }));
-            }
+            // if(event.message.fileName.includes("m4a")){
+            //     events_processed.push(bot.replyMessage(event.replyToken, {
+            //         type: "text",
+            //         text: "いい音ですので保存しておきますぞ！"
+            //     }));
+            // }
         }
     });
 
